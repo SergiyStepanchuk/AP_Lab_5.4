7a0a653dcd4417a68283c49604bd94e7037f2688
SergiyStepanchuk <serhii.stepanchuk.itis.2020@lpnu.ua>
2020-11-10 21:40:31 +0200

    init

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Program/main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		TEST_METHOD(met_S0)
+		{
+			Assert::IsFalse(abs(0.644021 - S0(10, 20)) > 0.00001, L"Incorrect func S0!");
+		}
+	};
+}
